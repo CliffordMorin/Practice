ce5667eca6eac91a31b5d700d70e9dca3c1f3c99
Clifford Morin <cemorin21@gmail.com>
2022-09-13 18:01:36 -0400

    added mult and map functions

--- fib.ex
+++ fib.ex
@@ -13,4 +13,11 @@ end
 defmodule Sum do 
     def sum([]), do: 0
     def sum([head | tail]), do: head + sum(tail)
+
+    def mult([]), do: 1
+    def mult([head | tail]), do: head * mult(tail)
+
+    def map([], _func), do: []
+    def map([head | tail], func) do [func.(head) | map(tail, func)] end
 end
+    
